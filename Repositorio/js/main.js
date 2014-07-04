ab6b523902909fa382bc8caa7d95f3e6969cdc0d
zexar <cesar.bboy@hotmail.com>
2014-07-04 17:33:42 -0500

    En Fases Final del index
    Aun falta agregarles las ventanas emergentes a las que faltan y
    optimizar  de alguna manera el js y el css para no repetir el codigo

--- Repositorio/js/main.js
+++ Repositorio/js/main.js
@@ -2,26 +2,56 @@ $(document).ready(function() {
 	$('#loginButton').click(function(){
    	 $('#Login_mostrar').slideToggle('fast');
 	});
-	
+
 	$('#Login_mostrar').mouseleave(function(){
 	 $(this).fadeOut();
 	});
-	
-	$('.clsImagen').click(function(){
-		$('.clsContenedor').css("width", "300px"); 
-	});
-	
-	$('.clsImagen').mouseleave(function(){
-		$('.clsContenedor').css("width", "0"); 
-	});
-	
-	$('.clsImagen').dblclick(function(){
-		$('.clsContenedor').css("width", "0"); 
-	});
 
-$('.nosotros').click(function(){lightbox_open()});
-$('.soporte').click(function(){lightbox_open()});
+	$('#fade').click(function(){lightbox_close();});
+	$('#light').click(function(){lightbox_close();});
+	$('#light1').click(function(){lightbox_close1();});
+	$('#fade').click(function(){lightbox_close1();});
+	$('#light2').click(function(){lightbox_close2();});
+	$('#fade').click(function(){lightbox_close2();});
+	$('#light3').click(function(){lightbox_close3();});
+	$('#fade').click(function(){lightbox_close3();});
+	$('#light4').click(function(){lightbox_close4();});
+	$('#fade').click(function(){lightbox_close4();});
+	$('#light5').click(function(){lightbox_close5();});
+	$('#fade').click(function(){lightbox_close5();});
+
+	$('.nosotros').click(function(){lightbox_open()});
+	$('.soporte').click(function(){lightbox_open()});
+
+	$('.item1').click(function(event){lightbox_open1(event)});
+	$('.item2').click(function(event){lightbox_open2(event)});
+	$('.item3').click(function(event){lightbox_open3(event)});
+	$('.item4').click(function(event){lightbox_open4(event)});
+	$('.item5').click(function(event){lightbox_open5(event)});
 });
+
+
+/*// Creamos un array vacio
+var ElementosClick = new Array();
+// Capturamos el click y lo pasamos a una funcion
+document.onclick = captura_click;
+	
+function captura_click(e) {
+	// Funcion para capturar el click del raton
+	var HaHechoClick;
+	if (e == null) {
+		// Si hac click un elemento, lo leemos
+		HaHechoClick = event.srcElement;
+	} else {
+		// Si ha hecho click sobre un destino, lo leemos
+		HaHechoClick = e.target;
+	}
+	// Añadimos el elemento al array de elementos
+	ElementosClick.push(HaHechoClick);
+	// Una prueba con salida en consola
+	console.log("Contenido sobre lo que ha hecho click: "+clickedElement.innerHTML);	
+}*/
+
 
 $(document).ready(function(){
   $(".login").click(function(event){
@@ -29,18 +59,68 @@ $(document).ready(function(){
     alert("Fuera de servicio")
   });
 });
+//item1
+function lightbox_open1(event){
+    window.scrollTo(0,0);
+    $('#light1').css("display","block");
+    $('#fade').css("display","block");
+};
+function lightbox_close1(){
+  	$('#light1').css("display","none");
+    $('#fade').css("display","none");
+};
+
+//item2
+function lightbox_open2(event){
+    window.scrollTo(0,0);
+    $('#light2').css("display","block");
+    $('#fade').css("display","block");
+};
+function lightbox_close2(){
+  	$('#light2').css("display","none");
+    $('#fade').css("display","none");
+};
+//item3
+function lightbox_open3(event){
+    window.scrollTo(0,0);
+    $('#light3').css("display","block");
+    $('#fade').css("display","block");
+};
+function lightbox_close3(){
+  	$('#light3').css("display","none");
+    $('#fade').css("display","none");
+};
+//item4
+function lightbox_open4(event){
+    window.scrollTo(0,0);
+    $('#light3').css("display","block");
+    $('#fade').css("display","block");
+};
+function lightbox_close4(){
+  	$('#light3').css("display","none");
+    $('#fade').css("display","none");
+};
+//item5
+function lightbox_open5(event){
+    window.scrollTo(0,0);
+    $('#light3').css("display","block");
+    $('#fade').css("display","block");
+};
+function lightbox_close5(){
+  	$('#light3').css("display","none");
+    $('#fade').css("display","none");
+};
 
 //Pantalla emergente con fondo oscuro
 function lightbox_open(){
     window.scrollTo(0,0);
-    document.getElementById('light').style.display='block';
-    document.getElementById('fade').style.display='block';  
-}
+    $('#light').css("display","block");
+    $('#fade').css("display","block");
+};
 function lightbox_close(){
-    document.getElementById('light').style.display='none';
-    document.getElementById('fade').style.display='none';
-}
-
+  	$('#light').css("display","none");
+    $('#fade').css("display","none");
+};
 //Reconocer el dispositivo desde donde se esta viendo el sitio y redieccionarlo
 function redireccionar(){
 var device = navigator.userAgent
